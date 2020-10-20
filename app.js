f01d54f90901b8b56fcbc1063da04fbb3e483b84
Polashahmad01 <dev.polashahmad@gmai.com>
2020-10-20 12:08:34 +0600

    Basic of Javascript

---
+++ app.js
@@ -0,0 +1,41 @@
+//Variable using VAR, LET, CONST;
+// console.log('Hello World');
+// let name = 'Polash';
+// console.log(name);
+// let firstName = 'Polash';
+// let lastName = 'Ahmad';
+// console.log(firstName, lastName);
+// const interestRate = 5;
+// console.log(interestRate);
+// let userName = 'Hello World';
+// let age = 30;
+// let hasSchool = true;
+// let firstNames = undefined;
+// let hasKids = null;
+// typeof operator
+
+// let person = {
+//     name: 'Polash',
+//     age: 20
+// }
+
+// person.name = 'Ahmad';
+// person[age] = 22;
+
+// console.log(person.age);
+
+// let selectedColors = ['green', 'blue'];
+// selectedColors[2] = 'Polash';
+// console.log(selectedColors);
+
+// function helloToConsole(firstName, lastName) {
+//     console.log('Hello ' + firstName + ' ' + lastName);
+// }
+
+// helloToConsole('Polash', 'Ahmad');
+
+// function square(number) {
+//     return number * number
+// }
+
+// console.log(square(6));
