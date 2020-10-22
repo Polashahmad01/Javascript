8fd6725c1bca1a73aafd58c9ba2a68fdf171e830
Istiak Ahmad Polash <58633156+Polashahmad01@users.noreply.github.com>
2020-10-22 12:50:27 +0600

    Learn About Control Flow

--- app.js
+++ app.js
@@ -118,3 +118,163 @@
 
 // console.log(a);
 // console.log(b);
+
+//==============================================================================================
+// Control Flow
+
+// let day = 'govt';
+
+// if(day === 1) {
+//     console.log('Holyday!')
+// } else if(day === 'govt') {
+//     console.log('It\' a govt holy Day');
+// } else {
+//     console.log('I have to go to office');
+// }
+
+// let role ='guest';
+
+// switch(role) {
+//     case 'guest':
+//         console.log('Guest User');
+//         break;
+//     case 'moderator':
+//         console.log('Moderator User');
+//         break;
+//     case 'general':
+//         console.log('General User');
+//         break;
+//     default:
+//         console.log('Faul user');
+
+// }
+
+// if(role === 'guest') {
+//     console.log('Guest User');
+// } else if(role === 'moderator') {
+//     console.log('Moderator User');
+// } else if(role === 'general') {
+//     console.log('General User');
+// } else {
+//     console.log('Faul User');
+// }
+
+// for(i = 1; i <= 5; i++) {
+//     if(i % 2 === 1) {
+//         console.log('Odd', i);
+//     } else {
+//         console.log('Even', i);
+//     }
+// }
+
+// for( i = 5; i >= 1; i--) {
+//     if(i % 2 === 1) {
+//     console.log('Odd', i);
+// } else {
+//     console.log('Even', i);
+//     }
+// }
+
+// let i = 5;
+
+// while(i <= 5) {
+//     if(i % 2 === 1) {
+//         console.log('Odd', i);
+//     } else {
+//         console.log('Even', i);
+//     }
+
+//     i++;
+// }
+
+// while(i >= 1) {
+//     if(i % 2 === 1) {
+//       console.log('Odd', i);
+//     } else {
+//   console.log('Even', i);
+//    }
+        
+// i--;
+// }
+
+
+// let i = 0;
+// do {
+//     if(i % 2 === 1) {
+//         console.log('Odd', i);
+//     } else {
+//         console.log('Even', i);
+//     }
+//     i++;
+// } while(i < 5);
+
+// let i = 5;
+// do {
+//     if(i % 2 === 0) {
+//         console.log('Even', i);
+//     } else {
+//         console.log('Odd', i);
+//     }
+//     i--;
+// } while( i >= 1);
+
+// const person = {
+//     name: 'Polash',
+//     age: 20
+// }
+
+// for(key in person) {
+//     console.log(person[key]);
+// }
+
+
+// const colors = ['red', 'green', 'blue', 'yellow'];
+
+// for(index in colors) {
+//     console.log(index, colors[index]);
+// }
+
+// for( color of colors) {
+//     console.log(color);
+// }
+
+
+// function maxNumber(num1, num2) {
+//     if(num1 > num2) {
+//         return num1;
+//     } else {
+//         return num2;
+//     }
+// }
+
+// console.log(maxNumber(100,2))
+
+// function isLandscape(width, height) {
+//     if(width > height) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+// console.log(isLandscape(1000, 200));
+
+const output = fizzBuzz(10);
+console.log(output);
+
+function fizzBuzz(input) {
+    if(typeof input !== 'number') {
+        console.log('Not a number');
+    } 
+    if((input % 3 === 0) && (input % 5 === 0)) {
+        console.log('FizzBuzz');
+    }
+    if(input % 3 === 0) {
+        console.log('Fizz');
+    }
+    if(input % 5 === 0) {
+        console.log('Buzz');
+    }
+
+    return input;
+}
